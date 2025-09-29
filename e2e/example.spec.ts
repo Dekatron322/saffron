40d4ce6258f7a4952d1f97a59acae826a5f29e5c
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-09-29 18:33:24 +0100

    Saffron Wellcare Pharmacy

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test"
 test("has title", async ({ page }) => {
   await page.goto("./")
 
-  await expect(page).toHaveTitle(/saffron/)
+  await expect(page).toHaveTitle(/Saffron Wellcare Pharmacy/)
 })
